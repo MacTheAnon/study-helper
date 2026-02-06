375e583781c9759638436e91ed9b264153304e85
MacTheAnon <mac1379530@icloud.com>
2026-02-05 18:03:12 -0600

    updated preload configuration

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,13 +1,22 @@
 import { contextBridge, ipcRenderer } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
+
+const api = {
+  getNotes: () => ipcRenderer.invoke('get-notes'),
+  openFilePicker: () => ipcRenderer.invoke('open-file-picker'),
+  deleteNote: (path) => ipcRenderer.invoke('delete-note', path),
+  saveCards: (cards) => ipcRenderer.invoke('save-cards', cards),
+  getCards: () => ipcRenderer.invoke('get-cards')
+}
 
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('studyHelperAPI', {
-      openFilePicker: () => ipcRenderer.invoke('open-file-picker'),
-      getNotes: () => ipcRenderer.invoke('get-notes'),
-      deleteNote: (path) => ipcRenderer.invoke('delete-note', path)
-    })
+    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('studyHelperAPI', api)
   } catch (error) {
     console.error(error)
   }
+} else {
+  window.electron = electronAPI
+  window.studyHelperAPI = api
 }
